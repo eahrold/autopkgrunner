fc91341e891d001132bce56559418d25c860c8f0
Eldon Ahrold <eldon.ahrold@gmail.com>
2014-06-17 06:21:15 -0500

    Initial Commit

---
+++ autopkgrunner/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  autopkgrunner
+//
+//  Created by Eldon on 6/17/14.
+//  Copyright (c) 2014 Eldon Ahrold. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
